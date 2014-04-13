f5f7147e74ae4f9738506a8fae2e9e728a6321dd
Vu Tuan Anh <terminal@Vus-Mac.local>
2014-04-14 00:05:11 +0700

    1404

---
+++ FurnitureDirect2u/CustomView/FavorCustomCell.h
@@ -0,0 +1,13 @@
+//
+//  FavorCustomCell.h
+//  FurnitureDirect2u
+//
+//  Created by Vu Tuan Anh on 4/14/14.
+//  Copyright (c) 2014 FurnitureDirect2u. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavorCustomCell : UITableViewCell
+
+@end
---
+++ FurnitureDirect2u/CustomView/FavorCustomCell.m
@@ -0,0 +1,34 @@
+//
+//  FavorCustomCell.m
+//  FurnitureDirect2u
+//
+//  Created by Vu Tuan Anh on 4/14/14.
+//  Copyright (c) 2014 FurnitureDirect2u. All rights reserved.
+//
+
+#import "FavorCustomCell.h"
+
+@implementation FavorCustomCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
